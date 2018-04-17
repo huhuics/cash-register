1a71d52e5de03c86130fe30b2f4ed21339f2aafd
Sunxiai51 <wuyiyang1992@qq.com>
2018-04-17 14:36:16 +0800

    添加springMVC拦截器配置

---
+++ src/main/java/cn/cash/register/common/CashRegisterConstants.java
@@ -0,0 +1,15 @@
+package cn.cash.register.common;
+
+/**
+ * 常量类
+ * 
+ * @author 51
+ * @version $Id: CashRegisterConstants.java, v 0.1 2018年4月17日 下午2:29:04 51 Exp $
+ */
+public class CashRegisterConstants {
+
+    /**
+     * 登录后存入session中的用户字段
+     */
+    public static final String LOGIN_FLAG = "user";
+}
---
+++ src/main/java/cn/cash/register/common/intercepter/CheckAdminIntercepter.java
@@ -0,0 +1,28 @@
+package cn.cash.register.common.intercepter;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.springframework.stereotype.Component;
+import org.springframework.web.servlet.handler.HandlerInterceptorAdapter;
+
+import cn.cash.register.common.CashRegisterConstants;
+
+/**
+ * 管理员登录拦截器
+ * 
+ * @author 51
+ * @version $Id: CheckAdminIntercepter.java, v 0.1 2018年4月17日 下午2:32:35 51 Exp $
+ */
+@Component
+public class CheckAdminIntercepter extends HandlerInterceptorAdapter {
+
+    @Override
+    public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler) throws Exception {
+        Object obj = request.getSession().getAttribute(CashRegisterConstants.LOGIN_FLAG); // 从session中获取用户信息
+        // TODO 51 配置拦截器逻辑
+        return true;
+
+    }
+
+}
