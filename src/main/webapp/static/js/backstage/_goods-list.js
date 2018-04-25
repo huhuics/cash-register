fd54a89dce08bee50a252db2b4bad6e01fddfa4c
Sunxiai51 <wuyiyang1992@qq.com>
2018-04-25 17:35:46 +0800

    后台商品添加jsp、js

--- src/main/webapp/static/js/backstage/_goods-list.js
+++ src/main/webapp/static/js/backstage/_goods-list.js
@@ -146,77 +146,135 @@ var vm = new Vue({
             supplierName: '',
             goodsTag: ''
         },
-        goods_categorys: [], // 全部分类
+    	goods_categorys: [], // 全部分类
         goods_brands: [], // 全部品牌
         goods_suppliers: [], // 全部供货商
         goods_tags: [], // 全部标签
         goods_units: [], // 全部单位
         goods_colors: [], // 全部颜色
         goods_sizes: [], // 全部尺码
-        displayImageUpload: false, // 是否显示图片上传框(仅编辑时显示)
-        goods: {
-            id: null,
-            goodsImageId: null,
-            goodsName: null,
-            barCode: null,
-            productNumber: null,
-            pinyinCode: null,
-            categoryName: null,
-            goodsStatus: null,
-            goodsBrand: null,
-            goodsColor: null,
-            goodsSize: null,
-            goodsTag: null,
-            goodsStock: null,
-            quantityUnit: null,
-            stockUpperLimit: null,
-            stockLowerLimit: null,
-            importPrice: null,
-            averageImportPrice: null,
-            salesPrice: null,
-            tradePrice: null,
-            vipPrice: null,
-            isVipDiscount: null,
-            supplierName: null,
-            productionDate: null,
-            qualityGuaranteePeriod: null,
-            isIntegral: null,
-            royaltyType: null,
-            isBooked: null,
-            isGift: null,
-            isWeigh: null,
-            isFixedPrice: null,
-            isTimeingPrice: null,
-            isHidden: null,
-            remark: null,
-            gmtUpdate: null,
-            gmtCreate: null
+        switches: {
+        	displayImageUpload: false, // 显示图片上传框开关(仅编辑时显示)
+            colorSize: false, // 颜色尺码开关
+            prodNumSame: false, // 货号和条码一致开关
+        },
+        goods: { // 商品实体
+        	id: null,
+        	goodsImageId: null,
+        	goodsName: null,
+        	barCode: null,
+        	productNumber: null,
+        	pinyinCode: null,
+        	categoryName: null,
+        	goodsStatus: true, // 默认启用
+        	goodsBrand: null,
+        	goodsColor: null,
+        	goodsSize: null,
+        	goodsTag: null,
+        	goodsStock: null,
+        	quantityUnit: null,
+        	stockUpperLimit: null,
+        	stockLowerLimit: null,
+        	lastImportPrice: null,
+        	averageImportPrice: null,
+        	salesPrice: null,
+        	tradePrice: null,
+        	vipPrice: null,
+        	isVipDiscount: true, // 默认开启会员折扣
+        	supplierName: null,
+        	productionDate: null,
+        	qualityGuaranteePeriod: null,
+        	isIntegral: null,
+        	royaltyType: null,
+        	isBooked: null,
+        	isGift: null,
+        	isWeigh: null,
+        	isFixedPrice: null,
+        	isTimeingPrice: null,
+        	isHidden: null,
+        	remark: null,
+        	//gmtUpdate: null,
+        	//gmtCreate: null
         }
+    },
+    computed: {
+    	goods_barCode() {
+    		return this.goods.barCode;
+    	},
+    	switches_colorSize() {
+    		return this.switches.colorSize;
+    	},
+    	switches_prodNumSame() {
+    		return this.switches.prodNumSame;
+    	},
+    	goods_isVipDiscount() {
+    		return this.goods.isVipDiscount;
+    	}
+    },
+    watch: {
+    	switches_prodNumSame: function() { // 货号与条码一致开关
+    		if(this.switches.prodNumSame) {
+    			this.goods.productNumber = this.goods.barCode;
+        		$("#productNumberInput").attr("readOnly","true");
+    		} else {
+    			$("#productNumberInput").removeAttr("readOnly");
+    		}
+    	},
+    	goods_barCode: function() { // 条码变化时可能需要更新货号
+    		if(this.switches.prodNumSame) {
+    			this.goods.productNumber = this.goods.barCode;
+    		}
+    	},
+    	switches_colorSize: function() { // 颜色尺码开关切换
+    		if(this.switches.colorSize) {
+    			$("#colorSizeBtn").removeAttr("disabled");
+    		} else {
+    			$("#colorSizeBtn").attr("disabled","true");
+    		}
+    	},
+    	goods_isVipDiscount: function() { // 会员折扣
+    		if(this.goods.isVipDiscount) {
+    			this.goods.vipPrice = null;
+        		$("#goodsVipPriceInput").attr("readOnly","true");
+    		} else {
+    			$("#goodsVipPriceInput").removeAttr("readOnly");
+    		}
+    	}
     },
     methods: {
         search: function() {
-            vm.reload();
+        	this.reload();
         },
         resetSearch: function() {
-            vm.reload();
+        	this.reload();
+        },
+        getBarCode: function() {
+        	$.ajax({
+                type: "GET",
+                url: basePath + "/admin/goods/getBarCode",
+                success: function(result) {
+                    if (result.code == "00") {
+                    	vm.goods.barCode = result.barCode;
+                    } else {
+                        layer.alert("条码生成失败：" + result.msg);
+                    }
+                }
+            });
         },
         add: function() {
             layer.open({
                 type: 1,
                 skin: 'layui-layer-lan',
                 title: "新增商品",
-                area: '600px',
+                area: '650px',
                 shadeClose: false,
                 content: jQuery("#goodsDiv"),
                 btn: ['提交', '取消'],
                 btn1: function(index) {
                     $.ajax({
                         type: "POST",
-                        url: basePath + "/admin/goods/addOrUpdate",
-                        data: {
-
-                        },
-                        dataType: "json",
+                        url: basePath + "/admin/goods/addGoodsInfo",
+                        data: vm.goods,
                         success: function(result) {
                             if (result.code == "00") {
                                 layer.alert('添加成功');
@@ -242,21 +300,27 @@ var vm = new Vue({
         exportGoods: function() {
 
         },
+        _editGoodsStock: function() { // 编辑库存
+        	
+        },
         reload: function() {
             var page = $("#jqGrid").jqGrid('getGridParam', 'page');
             $("#jqGrid").jqGrid('setGridParam', {
                 postData: {
-                    'barCode': vm.q.barCode,
-                    'goodsName': vm.q.goodsName,
-                    'pinyinCode': vm.q.pinyinCode,
-                    'goodsStatus': vm.q.goodsStatus,
-                    'categoryName': vm.q.categoryName,
-                    'goodsBrand': vm.q.goodsBrand,
-                    'supplierName': vm.q.supplierName,
-                    'goodsTag': vm.q.goodsTag
+                    'barCode': this.q.barCode,
+                    'goodsName': this.q.goodsName,
+                    'pinyinCode': this.q.pinyinCode,
+                    'goodsStatus': this.q.goodsStatus,
+                    'categoryName': this.q.categoryName,
+                    'goodsBrand': this.q.goodsBrand,
+                    'supplierName': this.q.supplierName,
+                    'goodsTag': this.q.goodsTag
                 },
                 page: page
             }).trigger("reloadGrid");
+        },
+        init: function() {
+        	
         },
         loadGoodsCategorys: function() {
         	// 加载所有商品分类列表
@@ -265,7 +329,7 @@ var vm = new Vue({
                 url: basePath + "/admin/goods/getGoodsCategoryTree",
                 success: function(result) {
                     if (result.code == "00") {
-                        vm.goods_categorys = result.tree;
+                        this.goods_categorys = result.tree;
                     } else {
                         layer.alert("加载商品分类列表出错" + result.msg);
                     }
@@ -279,7 +343,7 @@ var vm = new Vue({
         		url: basePath + "/admin/goods/queryAllGoodsBrand",
         		success: function(result) {
         			if (result.code == "00") {
-        				vm.goods_brands = result.brands;
+        				this.goods_brands = result.brands;
         			} else {
         				layer.alert("加载商品品牌列表出错" + result.msg);
         			}
@@ -293,7 +357,7 @@ var vm = new Vue({
         		url: basePath + "/admin/goods/queryAllGoodsBrand",
         		success: function(result) {
         			if (result.code == "00") {
-        				vm.goods_suppliers = result.suppliers;
+        				this.goods_suppliers = result.suppliers;
         			} else {
         				layer.alert("加载商品供货商列表出错" + result.msg);
         			}
@@ -307,7 +371,7 @@ var vm = new Vue({
         		url: basePath + "/admin/goods/queryAllGoodsTag",
         		success: function(result) {
         			if (result.code == "00") {
-        				vm.goods_tags = result.tags;
+        				this.goods_tags = result.tags;
         			} else {
         				layer.alert("加载商品标签列表出错" + result.msg);
         			}
@@ -321,7 +385,7 @@ var vm = new Vue({
         		url: basePath + "/admin/goods/queryAllGoodsUnit",
         		success: function(result) {
         			if (result.code == "00") {
-        				vm.goods_units = result.units;
+        				this.goods_units = result.units;
         			} else {
         				layer.alert("加载商品单位列表出错" + result.msg);
         			}
@@ -335,7 +399,7 @@ var vm = new Vue({
         		url: basePath + "/admin/goods/queryAllGoodsColor",
         		success: function(result) {
         			if (result.code == "00") {
-        				vm.goods_colors = result.colors;
+        				this.goods_colors = result.colors;
         			} else {
         				layer.alert("加载商品颜色列表出错" + result.msg);
         			}
@@ -349,7 +413,7 @@ var vm = new Vue({
         		url: basePath + "/admin/goods/queryAllGoodsSize",
         		success: function(result) {
         			if (result.code == "00") {
-        				vm.goods_sizes = result.sizes;
+        				this.goods_sizes = result.sizes;
         			} else {
         				layer.alert("加载商品尺寸列表出错" + result.msg);
         			}
@@ -358,6 +422,7 @@ var vm = new Vue({
         }
     },
     mounted: function() {
+    	this.init();
 //    	this.loadGoodsCategorys();
 //    	this.loadGoodsBrands();
 //    	this.loadGoodsSuppliers();
@@ -365,5 +430,6 @@ var vm = new Vue({
 //    	this.loadGoodsUnits();
 //    	this.loadGoodscolors();
 //    	this.loadGoodsSizes();
+    	
     }
 });
