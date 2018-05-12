00f9424f7940c1f17d08460fe9d20e5b6a459206
Sunxiai51 <wuyiyang1992@qq.com>
2018-05-13 04:18:21 +0800

    解决冲突

--- src/main/java/cn/cash/register/controller/backstage/SalesController.java
+++ src/main/java/cn/cash/register/controller/backstage/SalesController.java
@@ -10,14 +10,17 @@ import java.util.Map;
 import javax.annotation.Resource;
 
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
+import com.github.pagehelper.PageInfo;
+
 import cn.cash.register.common.request.ExchangeJobQueryRequest;
 import cn.cash.register.common.request.SalesBasicFactsQueryRequest;
+import cn.cash.register.common.request.TradeGoodsDetailQueryRequest;
 import cn.cash.register.dao.domain.ExchangeJobDetail;
+import cn.cash.register.dao.domain.GoodsSaleStatistics;
 import cn.cash.register.dao.domain.SalesBasicFacts;
 import cn.cash.register.service.ExchangeJobService;
 import cn.cash.register.service.SalesService;
@@ -40,14 +43,6 @@ public class SalesController {
     private SalesService       salesService;
 
     /**
-     * 查询交接班记录页面
-     */
-    @GetMapping(value = "/queryExchangeJobs")
-    public String queryExchangeJobsPage() {
-        return "backstage/_sales-exchangeJobs";
-    }
-
-    /**
      * 查询交接班记录
      */
     @ResponseBody
@@ -55,14 +50,6 @@ public class SalesController {
     public ResultSet queryExchangeJobs(ExchangeJobQueryRequest request) {
         List<ExchangeJobDetail> exchangeJobs = exchangeJobService.query(request);
         return ResultSet.success().put("exchangeJobs", exchangeJobs);
-    }
-
-    /**
-     * 查询营业概况页面
-     */
-    @GetMapping(value = "/queryBasicFacts")
-    public String queryBasicFactsPage() {
-        return "backstage/_sales-basicFacts";
     }
 
     /**
@@ -77,11 +64,13 @@ public class SalesController {
     }
 
     /**
-     * 查询销售单据页面
+     * 查询商品销售统计
      */
-    @GetMapping(value = "/tradeDetail")
-    public String salesTradeDetailList() {
-        return "backstage/_sales-tradeDetail-list";
+    @ResponseBody
+    @PostMapping(value = "/queryGoodsSaleStatistics")
+    public ResultSet queryGoodsSaleStatistics(TradeGoodsDetailQueryRequest request) {
+        PageInfo<GoodsSaleStatistics> ret = salesService.queryGoodsSaleStatistics(request);
+        return ResultSet.success().put("ret", ret);
     }
 
 }
