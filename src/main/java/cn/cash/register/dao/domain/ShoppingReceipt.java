6cbaf953bb9cec3f748921927b970942b681f3ba
huhuics <hhuics@163.com>
2018-05-25 19:48:19 +0800

    完善

--- src/main/java/cn/cash/register/dao/domain/ShoppingReceipt.java
+++ src/main/java/cn/cash/register/dao/domain/ShoppingReceipt.java
@@ -32,5 +32,88 @@ public class ShoppingReceipt extends BaseDomain {
 
     private String                 sellerNo;
 
+    private String                 change;                                  //找零
+
     private List<TradeGoodsDetail> goodsInfo;
+
+    public String getTradeNo() {
+        return tradeNo;
+    }
+
+    public void setTradeNo(String tradeNo) {
+        this.tradeNo = tradeNo;
+    }
+
+    public String getTradeTime() {
+        return tradeTime;
+    }
+
+    public void setTradeTime(String tradeTime) {
+        this.tradeTime = tradeTime;
+    }
+
+    public String getMemberNo() {
+        return memberNo;
+    }
+
+    public void setMemberNo(String memberNo) {
+        this.memberNo = memberNo;
+    }
+
+    public String getGoodsCount() {
+        return goodsCount;
+    }
+
+    public void setGoodsCount(String goodsCount) {
+        this.goodsCount = goodsCount;
+    }
+
+    public String getTotalAmount() {
+        return totalAmount;
+    }
+
+    public void setTotalAmount(String totalAmount) {
+        this.totalAmount = totalAmount;
+    }
+
+    public String getGoodsDiscount() {
+        return goodsDiscount;
+    }
+
+    public void setGoodsDiscount(String goodsDiscount) {
+        this.goodsDiscount = goodsDiscount;
+    }
+
+    public String getTotalActualAmount() {
+        return totalActualAmount;
+    }
+
+    public void setTotalActualAmount(String totalActualAmount) {
+        this.totalActualAmount = totalActualAmount;
+    }
+
+    public String getSellerNo() {
+        return sellerNo;
+    }
+
+    public void setSellerNo(String sellerNo) {
+        this.sellerNo = sellerNo;
+    }
+
+    public List<TradeGoodsDetail> getGoodsInfo() {
+        return goodsInfo;
+    }
+
+    public void setGoodsInfo(List<TradeGoodsDetail> goodsInfo) {
+        this.goodsInfo = goodsInfo;
+    }
+
+    public String getChange() {
+        return change;
+    }
+
+    public void setChange(String change) {
+        this.change = change;
+    }
+
 }
